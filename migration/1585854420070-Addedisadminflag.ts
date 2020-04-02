d9aea0dae7ca5c8b1004222186838de30753bbb9
Ilya Danilov <peertosir@gmail.com>
2020-04-02 22:09:12 +0300

    Fix migration

--- migration/1585854420070-Addedisadminflag.ts
+++ migration/1585854420070-Addedisadminflag.ts
@@ -1,14 +1,16 @@
 import {MigrationInterface, QueryRunner} from "typeorm";
 
-export class AddedIsAdminFlag1585847034096 implements MigrationInterface {
-    name = 'AddedIsAdminFlag1585847034096'
+export class Addedisadminflag1585854420070 implements MigrationInterface {
+    name = 'Addedisadminflag1585854420070'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`ALTER TABLE "project" ALTER COLUMN "urls" SET DEFAULT '{}'::text[]`, undefined);
+        await queryRunner.query(`ALTER TABLE "user" ADD "is_admin" boolean NOT NULL DEFAULT false`, undefined);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`ALTER TABLE "project" ALTER COLUMN "urls" SET DEFAULT '{}'`, undefined);
+        await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "is_admin" boolean NOT NULL DEFAULT false`, undefined);
     }
 
 }
